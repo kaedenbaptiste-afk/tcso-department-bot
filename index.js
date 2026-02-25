275b14b39619c83be60574c3ab5122b3a2bd258a
kaedenbaptiste-afk <kaedenbaptiste@gmail.com>
2026-02-24 23:10:45 -0500

    Initialize Discord bot with basic setup

---
+++ index.js
@@ -0,0 +1,14 @@
+const { Client, GatewayIntentBits } = require('discord.js');
+
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMembers
+  ]
+});
+
+client.once('ready', () => {
+  console.log(`TCSO Bot Online as ${client.user.tag}`);
+});
+
+client.login(process.env.DISCORD_TOKEN);
